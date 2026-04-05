69dddfe6bef9d690dbe8d13a25a6d89fdcc0abf3
Jesse Roland <j.roland277@gmail.com>
2026-04-05 13:52:06 -0500

    init nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Transit app development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            
+            # python 3
+            python3
+
+            # Development tools
+            git
+            uv
+          ];
+        };
+      }
+    );
+}
